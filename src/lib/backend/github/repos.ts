1f90bbb60162eb58f3f3854c8fecf6f13e60506b
COD3VIL <codebase65@outlook.com>
2024-05-12 14:50:01 +0100

    peformance improvements

--- src/lib/backend/github/repos.ts
+++ src/lib/backend/github/repos.ts
@@ -24,47 +24,51 @@ export async function getRepoCommitCount(owner: string, repo: string) {
 }
 
 export async function getReposStats() {
-    const { data } = await octokit.rest.repos.listForAuthenticatedUser();
+    try {
+        const { data } = await octokit.rest.repos.listForAuthenticatedUser();
 
-    const repos: { fork: boolean; created_at: string | null; forks: number; isArchived: boolean; isDisabled: boolean; name: string; desc: string | null; issues: number; stars: number; language: string | null; visibility: string | undefined; }[] = []
-    let totalStars = 0
-    let totalForks = 0
-    let totalIssues = 0
-    let totalCommitCounts = 0
-    const tempData = data
+        const repos: { fork: boolean; created_at: string | null; forks: number; isArchived: boolean; isDisabled: boolean; name: string; desc: string | null; issues: number; stars: number; language: string | null; visibility: string | undefined; }[] = []
+        let totalStars = 0
+        let totalForks = 0
+        let totalIssues = 0
+        let totalCommitCounts = 0
+        const tempData = data
 
-    for (let index = 0; index < tempData.length - 1; index++) {
-        const element = tempData[index];
-        if (!element.archived && !element.disabled) {
+        for (let index = 0; index < tempData.length - 1; index++) {
+            const element = tempData[index];
+
             const commits = await getRepoCommitCount('michaelnji', element.name)
             totalCommitCounts = totalCommitCounts + commits
+
+
         }
 
-    }
+        data.forEach((repo) => {
 
-    data.forEach((repo) => {
-        if (!repo.archived && !repo.disabled) {
             totalStars = totalStars + repo.stargazers_count
             totalForks = totalForks + repo.forks_count
             totalIssues = totalIssues + repo.open_issues_count
-        }
-        repos.push({
-            fork: repo.fork,
-            created_at: repo.created_at,
-            forks: repo.forks_count,
-            isArchived: repo.archived,
-            isDisabled: repo.disabled,
-            name: repo.name,
-            desc: repo.description,
-            issues: repo.open_issues_count,
-            stars: repo.stargazers_count,
-            language: repo.language,
-            visibility: repo.visibility
+
+            repos.push({
+                fork: repo.fork,
+                created_at: repo.created_at,
+                forks: repo.forks_count,
+                isArchived: repo.archived,
+                isDisabled: repo.disabled,
+                name: repo.name,
+                desc: repo.description,
+                issues: repo.open_issues_count,
+                stars: repo.stargazers_count,
+                language: repo.language,
+                visibility: repo.visibility
+            })
         })
-    })
-    console.log(totalCommitCounts)
-    return {
-        repos, totalForks, totalIssues, totalStars, totalCommitCounts
+
+        return {
+            repos, totalForks, totalIssues, totalStars, totalCommitCounts
+        }
+    } catch (error) {
+        return null
     }
 }
 
