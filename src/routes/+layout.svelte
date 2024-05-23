48de7cd4c56b61d4dc3a91adf5e9056c5c28355f
COD3VIL <codebase65@outlook.com>
2024-05-23 18:08:19 +0100

    perf: ⚡️ added partytown to handle third party scripts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,18 @@
 <script>
-	import { UmamiAnalytics } from '@lukulent/svelte-umami';
-	import { fly, fade } from 'svelte/transition';
-	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 	import { afterNavigate, beforeNavigate, onNavigate } from '$app/navigation';
 	import { getOrSetItem } from '$lib/scripts/dbManager';
 	import theme from '$lib/stores/theme';
+	import { partytownSnippet } from '@builder.io/partytown/integration';
+	import '@fontsource/short-stack';
+	import '@fontsource/ubuntu-mono';
+	import { UmamiAnalytics } from '@lukulent/svelte-umami';
+	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 	import { inject } from '@vercel/analytics';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import 'animate.css';
 	import { onMount } from 'svelte';
 	import Navbar from './../lib/components/navigation/navbar.svelte';
 	import './styles.postcss';
-	import '@fontsource/short-stack';
-	import '@fontsource/ubuntu-mono';
-	import { partytownSnippet } from '@builder.io/partytown/integration';
 	let isLoading = false;
 	beforeNavigate(({ to }) => (isLoading = !!to?.route.id));
 	afterNavigate(() => setTimeout(() => (isLoading = false), 200));
@@ -46,7 +45,7 @@
 	</script>
 	{@html '<script>' + partytownSnippet() + '</script>'}
 
-	<script
+	<script defer
 		type="text/partytown"
 		src="https://cloud.umami.is/script.js"
 		data-website-id="7b096ad9-1f68-4172-aabc-cdbd04437982"
@@ -55,7 +54,7 @@
 		type="text/partytown"
 		src="https://www.googletagmanager.com/gtag/js?id=G-B22J642CLK"
 	></script>
-	<script type="text/partytown">
+	<script async type="text/partytown">
 		window.dataLayer = window.dataLayer || [];
 		function gtag() {
 			dataLayer.push(arguments);
